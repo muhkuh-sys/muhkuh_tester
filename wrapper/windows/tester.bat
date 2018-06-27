f595b0c147625a04d1186969c0a469b7b382c19b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-27 17:29:01 +0200

    Enclose variables in percent.

--- wrapper/windows/tester.bat
+++ wrapper/windows/tester.bat
@@ -2,4 +2,4 @@ rem Run the lua5.1.exe in the path of this scrip file. Here is a way how to get 
 rem https://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows
 SET SELFBAT=%~dp0
 SET SELFDIR=%SELFBAT:~0,-1%
-%SELFDIR\lua5.1 %SELFDIR\system.lua %*
+%SELFDIR%\lua5.1 %SELFDIR%\system.lua %*
