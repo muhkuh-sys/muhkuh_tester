e5dfd4dc747ab1a0b9c26570ed9cfaa43e5ef264
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-07-17 12:16:35 +0200

    Add a wrapper script for powershell.

--- installer/jonchki/lua5.1/install.lua
+++ installer/jonchki/lua5.1/install.lua
@@ -289,6 +289,7 @@ t:install('doc/', '${install_doc}/')
 -- Copy the wrapper.
 if strDistId=='windows' then
   t:install('wrapper/windows/tester.bat',  '${install_executables}/')
+  t:install('wrapper/windows/tester.ps1',  '${install_executables}/')
 elseif strDistId=='ubuntu' then
   t:install('wrapper/linux/tester',        '${install_executables}/')
 else
